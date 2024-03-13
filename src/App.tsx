df82bf233bb8d6062447f5c9ce0831c2fb687704
yanyanmaru <manmarutech@gmail.com>
2024-03-14 04:10:52 +0900

    通知のタイミングを変更

--- src/App.tsx
+++ src/App.tsx
@@ -3,32 +3,19 @@ import { useEffect, useState } from "react";
 import { Timer } from "./components/Timer.js";
 
 
-
 function App() {
   const [isDisplay, setIsDisplay] = useState(false);
 
-  function notification() {
-    setIsDisplay(!isDisplay);
-    switch (Notification.permission) {
-      case "default":
-        Notification.requestPermission();
-        break;
-      case "granted":
-        new Notification("Code Tips", {
-          // ここを追加
-          body: "最新のニュースをお知らせします",
-        });
-        break;
-      case "denied":
-        alert("通知が拒否されています");
-        break;
-    }
+  function toggleButton() {
+    setIsDisplay(!isDisplay)
+    
   }
+  
 
 
   return (
     <div>
-      <button onClick={notification}>
+      <button onClick={toggleButton}>
         {isDisplay ? "終了" : "開始"}
       </button>
       {isDisplay && <Timer setIsDisplay={setIsDisplay} />}
--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -11,6 +11,20 @@ export const Timer: React.FC<{
       console.log("再レンダー");
       if (count < 0) {
         setIsDisplay(false);
+        switch (Notification.permission) {
+            case "default":
+              Notification.requestPermission();
+              break;
+            case "granted":
+              new Notification("Code Tips", {
+                // ここを追加
+                body: "最新のニュースをお知らせします",
+              });
+              break;
+            case "denied":
+              alert("通知が拒否されています");
+              break;
+          }
         return;
       }
   
